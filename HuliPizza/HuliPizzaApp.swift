271efaebaa0399f02f700abc8583812903b95786
Momin Shoaib Ahmed <momin.shoaib14@gmail.com>
2023-06-13 10:38:00 +0500

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Momin Shoaib on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
